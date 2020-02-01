24617d819a15901485ddc4dccc40649efbb69fe2
olie <olie.smit0@gmail.com>
2020-02-01 15:04:28 +0000

    Update Cmake.

---
+++ Source/WorldGeneration.hpp
@@ -0,0 +1,22 @@
+#ifndef WORLD_GENERATION_HPP
+#define WORLD_GENERATION_HPP
+
+#include <vector>
+
+enum BoundryConditions
+{
+	Brick = 1,
+	Empty = 1 << 1
+};
+
+class WorldForge
+{
+public:
+
+
+private:
+
+};
+
+
+#endif
