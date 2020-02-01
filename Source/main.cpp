67f9789d0e129b117cc2210c8085225e10b8b047
olie <olie.smit0@gmail.com>
2020-02-01 16:21:33 +0000

    Render the first brick.

--- Source/main.cpp
+++ Source/main.cpp
@@ -15,6 +15,9 @@ int main()
 
 	Engine eng(window);
 
+	eng.startFrame();
+	bool firstFrame = true;
+
 	StaticMesh* firstMesh = new StaticMesh(
 		"Assets\\Meshes\\BasicBrick.fbx",
 		VertexAttributes::Position4 |
@@ -24,16 +27,55 @@ int main()
 	);
 
 	Scene testScene("Assets\\Materials");
-	//testScene.loadFromFile(VertexAttributes::Position4 | VertexAttributes::Normals | VertexAttributes::TextureCoordinates | VertexAttributes::Material, &engine);
-	//testScene.loadSkybox(skybox, &engine);
+
+	std::array<std::string, 6> skybox{ "./Assets/Skybox.png", "./Assets/Skybox.png", "./Assets/Skybox.png", "./Assets/Skybox.png", "./Assets/Skybox.png", "./Assets/Skybox.png" };
+	testScene.loadSkybox(skybox, &eng);
 	testScene.setShadowingLight(float3(10.0f, -10.0f, 10.0f), float3(0.0f, 0.0f, 1.0f));
+	const auto meshID = testScene.addMesh(*firstMesh, MeshType::Dynamic);
+	testScene.addMeshInstance(meshID, glm::mat4(1.0f));
 	testScene.loadMaterials(&eng);
 	testScene.finalise(&eng);
 
 	eng.setScene(testScene);
 
+	eng.registerPass(PassType::GBuffer);
+	eng.registerPass(PassType::DeferredPBRIBL);
+	eng.registerPass(PassType::DFGGeneration);
+	eng.registerPass(PassType::Skybox);
+	eng.registerPass(PassType::ConvolveSkybox);
+	eng.registerPass(PassType::Composite);
+
 	while (!glfwWindowShouldClose(window))
 	{
 		glfwPollEvents();
+
+		if (!firstFrame)
+			eng.startFrame();
+
+		firstFrame = false;
+
+		Camera& camera = eng.getCurrentSceneCamera();
+
+		if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+			camera.moveForward(0.2f);
+		if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+			camera.moveBackward(0.2f);
+		if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+			camera.moveLeft(0.2f);
+		if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+			camera.moveRight(0.2f);
+		if (glfwGetKey(window, GLFW_KEY_Q) == GLFW_PRESS)
+			camera.rotateYaw(1.0f);
+		if (glfwGetKey(window, GLFW_KEY_E) == GLFW_PRESS)
+			camera.rotateYaw(-1.0f);
+		if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS)
+			camera.moveUp((0.2f));
+		if (glfwGetKey(window, GLFW_KEY_C) == GLFW_PRESS)
+			camera.moveDown(0.2f);
+
+		eng.recordScene();
+		eng.render();
+		eng.swap();
+		eng.endFrame();
 	}
 }
