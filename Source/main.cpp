57b5eb8943d0b134bafb4739fc649bf98dfce44f
Edd Sherlock <pulsarflux@googlemail.com>
2020-02-01 15:31:50 +0000

    Test loading a mesh

--- Source/main.cpp
+++ Source/main.cpp
@@ -2,6 +2,8 @@
 #include "Engine/Engine.hpp"
 #include "GLFW/glfw3.h"
 
+#include "Engine/StaticMesh.h"
+
 int main()
 {
 	glfwInit();
@@ -12,6 +14,23 @@ int main()
 	auto* window = glfwCreateWindow(1600, 900, "wall", nullptr, nullptr);
 
 	Engine eng(window);
+
+	StaticMesh* firstMesh = new StaticMesh(
+		"Assets\\Meshes\\BasicBrick.fbx",
+		VertexAttributes::Position4 |
+		VertexAttributes::TextureCoordinates |
+		VertexAttributes::Normals,
+		0
+	);
+
+	Scene testScene("Materials");
+	//testScene.loadFromFile(VertexAttributes::Position4 | VertexAttributes::Normals | VertexAttributes::TextureCoordinates | VertexAttributes::Material, &engine);
+	//testScene.loadSkybox(skybox, &engine);
+	testScene.setShadowingLight(float3(10.0f, -10.0f, 10.0f), float3(0.0f, 0.0f, 1.0f));
+	testScene.loadMaterials(&eng);
+	testScene.finalise(&eng);
+
+	eng.setScene(testScene);
 
 	while (!glfwWindowShouldClose(window))
 	{
