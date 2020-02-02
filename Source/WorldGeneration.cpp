8fb3a6c2e21197a97149649f9684c39d98ae44d0
olie <olie.smit0@gmail.com>
2020-02-02 21:43:04 +0000

    Update world building.

--- Source/WorldGeneration.cpp
+++ Source/WorldGeneration.cpp
@@ -1,1 +1,35 @@
 #include "WorldGeneration.hpp"
+
+#include <algorithm>
+#include <random>
+
+
+void WorldForge::collapse()
+{
+	std::vector<Node> tmpNodes;
+	tmpNodes.reserve(mNodeSet.size());
+
+	for (size_t x = 0; x < xSize; ++x)
+	{
+		for (size_t y = 0; y < ySize; ++y)
+		{
+			const size_t index = (x * ySize) + y;
+			const Node& currentNode = mNodeSet[mWorldNodes[index]];
+
+			std::copy_if(mNodeSet.begin(), mNodeSet.end(), tmpNodes.begin(), [&](const Node& node)
+			{
+				return true; // TODO add a proper predicate.
+			});
+
+			const uint32_t randomIndex = rand() % tmpNodes.size();
+			Node newNode = tmpNodes[randomIndex];
+
+			//const auto it = std::find(mNodeSet.begin(), mNodeSet.end(), newNode);
+			//const auto newIndex = std::distance(mNodeSet.begin(), it);
+
+			//mWorldNodes[index] = newIndex;
+
+		}
+		tmpNodes.clear();
+	}
+}
--- Source/WorldGeneration.hpp
+++ Source/WorldGeneration.hpp
@@ -3,19 +3,45 @@
 
 #include <vector>
 
-enum BoundryConditions
+enum BoundryConditions : uint32_t
 {
 	Brick = 1,
 	Empty = 1 << 1
 };
 
+
+struct Node
+{
+	BoundryConditions mConditions[6][4];
+};
+
+
 class WorldForge
 {
 public:
 
+	WorldForge(const size_t x, const size_t y) :
+		mNodeSet{},
+		xSize{x},
+		ySize{y},
+		mWorldNodes(x * y) 
+	{
+	}
+
+	void addNode(const Node& node)
+	{
+		mNodeSet.push_back(node);
+	}
+
+	void collapse();
 
 private:
 
+	std::vector<Node> mNodeSet;
+
+	size_t xSize;
+	size_t ySize;
+	std::vector<uint32_t> mWorldNodes;
 };
 
 
